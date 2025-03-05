b5a9029e7d4b96e29f1b9c7fdd9aa8acf817b4a4
papnoemi199999 <papnoemi199999@gmail.com>
2025-03-05 22:24:19 +0200

    error: location set to 1 in VertexShaderSource string

--- Szeminarium1/Program.cs
+++ Szeminarium1/Program.cs
@@ -13,7 +13,7 @@ namespace Szeminarium1
 
         private static readonly string VertexShaderSource = @"
         #version 330 core
-        layout (location = 0) in vec3 vPos;
+        layout (location = 1) in vec3 vPos;
 		layout (location = 1) in vec4 vCol;
 
 		out vec4 outCol;
